e15a4a3d75cafc63e0217cba18d403c207e7a0d1
ilmoroz <ilmoroz@outlook.com>
2021-03-01 00:30:25 +0300

    Блок Профиля РТС и Метро, Подготовка блока МП, СТ, Настроек

--- src/views/lk/ProfileMT.vue
+++ src/views/lk/ProfileMT.vue
@@ -1,4 +1,44 @@
 <template>
-<h1>Профиль Метро</h1>
-
+    <div class="lk-profile">
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+                <div class="col-lg-5">
+                    <div class="lk-profile-block lk-monbox">
+                        <div class="lk-profile-icon"> <img src="../../assets/img/lk/icon/icon_profile.png" alt=""></div>
+                        <div class="lk-profile-nickname"> Rise </div>
+                        <div class="lk-profile-type"> Обычный игрок </div>
+                        <div class="lk-profile-info"> 
+                            <ul>
+                                <li>
+                                    Должность:
+                                    <span> пельмешек </span>
+                                </li>
+                                <li>
+                                    Атрибутивы:
+                                    <span> № 0001 </span>
+                                </li>
+                                <li>
+                                    Последняя активность:
+                                    <span> 04.11.20 </span>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="lk-profile-shoulder">
+                            <ul>
+                                <span>Погоны</span>
+                                <li>
+                                    <img src="../../assets/img/lk/icon/shoulder-1.png">
+                                    <span>Машинист I класса</span>
+                                </li>
+                                <li>
+                                    <img src="../../assets/img/lk/icon/shoulder-1.png">
+                                    <span>Поездной диспетчер</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
--- src/views/lk/ProfileRTS.vue
+++ src/views/lk/ProfileRTS.vue
@@ -1,5 +1,37 @@
 <template>
-
-<h1>Профиль РТС</h1>
-
+    <div class="lk-profile">
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+                <div class="col-lg-5">
+                    <div class="lk-profile-block lk-monbox">
+                        <div class="lk-profile-icon"> <img src="../../assets/img/lk/icon/icon_profile.png" alt=""></div>
+                        <div class="lk-profile-nickname"> Louis La Roshelle </div>
+                        <div class="lk-profile-type"> Обычный игрок </div>
+                        <div class="lk-profile-shoulder">
+                            <ul>
+                                <span>Должность</span>
+                                <li>
+                                    <img src="../../assets/img/lk/icon/shoulder-1.png">
+                                    <span>Машинист I класса</span>
+                                </li>
+                                <li>
+                                    <img src="../../assets/img/lk/icon/shoulder-1.png">
+                                    <span>Поездной диспетчер</span>
+                                </li>
+                                <li>
+                                    <img src="../../assets/img/lk/icon/shoulder-1.png">
+                                    <span>Организатор МП</span>
+                                </li>
+                                <li>
+                                    <img src="../../assets/img/lk/icon/shoulder-1.png">
+                                    <span>Администратор</span>
+                                </li>
+                                <p>Почта: <span>ilmoroz@outlook.com</span></p>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
